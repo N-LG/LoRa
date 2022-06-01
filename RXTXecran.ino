f7f53df836710be52b04ae33da46c59153a624d3
Nicolas LG <34491677+N-LG@users.noreply.github.com>
2022-06-02 01:21:07 +0200

    code fonctionnel mais a optimiser sans doute un peu

--- RXTXecran.ino
+++ RXTXecran.ino
@@ -1,6 +1,6 @@
 /*********
 modifié a partir des codes d'exemples et des explications trouvé a https://randomnerdtutorials.com/esp32-lora-rfm95-transceiver-arduino-ide/
-
+code d'envoie/reçeption de trame Lora avec rapport sur ecran et liaison COM
 
 *********/
 
@@ -21,11 +21,31 @@ Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 
 int counter = 0;
+String LoRaData = "";
+int LoraRSSI = 0;
+int LoRaSNR = 0;
 
 void setup() {
   Serial.begin(115200);
+
+//initialise l'écran
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+  display.clearDisplay();
+  display.setCursor(0,0);
+
+  display.print("init module RFM95");
+  display.display(); 
+
+  
   //configure le brochage du module lora RFM95
   LoRa.setPins(ss, rst, dio0);
+Serial.println("init carte");
+  display.println("init carte");
+  display.display(); 
+
   
   //configure la fréquence du module https://www.thethingsnetwork.org/docs/lorawan/frequencies-by-country/
   //433E6 433Mhz 
@@ -33,6 +53,9 @@ void setup() {
   //penser a adapter la taille de l'antenne: 86,3mm pour 868Mhz et 173,1mm pour 433Mhz
   while (!LoRa.begin(866E6)) {
     Serial.println(".");
+      display.print(".");
+  display.display(); 
+
     delay(500);
   }
 
@@ -41,14 +64,12 @@ void setup() {
   LoRa.setSyncWord(0x89);
   Serial.println("carte démarré!");
 
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
-  display.setTextSize(2);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
 
   display.clearDisplay();
   display.setCursor(0,0);
   display.print("Testeur");
+  display.setCursor(0,16);
   display.print("LoRa: ok");
   display.display(); 
 
@@ -65,15 +86,17 @@ void loop() {
 
     // lit le paquet
     while (LoRa.available()) {
-      String LoRaData = LoRa.readString();
-      Serial.print(LoRaData); 
+LoRaData = LoRa.readString();
+      Serial.println(LoRaData); 
     }
 
     // lit information sur la qualité du signal
+ LoraRSSI = LoRa.packetRssi(); //Received Signal Strength Indicator   https://www.thethingsnetwork.org/docs/lorawan/rssi-and-snr/
+ LoRaSNR = LoRa.packetSnr(); //Signal-to-Noise Ratio
     Serial.print(" RSSI:");
-    Serial.println(LoRa.packetRssi());  //Received Signal Strength Indicator   https://www.thethingsnetwork.org/docs/lorawan/rssi-and-snr/
+    Serial.println(LoraRSSI);  
     Serial.print(" SNR:");
-    Serial.println(LoRa.packetSnr());   //Signal-to-Noise Ratio
+    Serial.println(LoRaSNR);   
   }
 
 
@@ -82,9 +105,26 @@ void loop() {
 
   //Send LoRa packet to receiver
   LoRa.beginPacket();
-  LoRa.print("Salut ");
   LoRa.print(counter);
   LoRa.endPacket();
+
+
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print("TX:");
+  display.print(counter);
+    display.setCursor(0,16);
+   display.print("RX:");
+  display.print(LoRaData);
+    display.setCursor(0,32);
+     display.print("RSSI:");
+    display.println(LoraRSSI);
+      display.setCursor(0,48);
+   display.print("SNR:");
+   display.println(LoRaSNR);   
+  display.display(); 
+
+
 
   counter++;
 
